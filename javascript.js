b5a337ea70918b9ad9d793ae90ac61b8519750a2
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-11-06 15:43:09 -0600

    Made form validation

--- javascript.js
+++ javascript.js
@@ -1,131 +1,178 @@
-var registerBtn = document.querySelector("#register");
-var loginBtn = document.querySelector("#login");
-var registerForm = document.querySelector(".register");
-var mainContent = document.getElementById("main-content");
-var loginDiv = document.querySelector(".login");
-var register = document.querySelector(".register-btn");
-var login = document.querySelector(".login-btn");
-var profile = document.querySelector("#profile");
-var userName = document.querySelector("#username");
-var passWord = document.querySelector("#password");
-var passwordRepeat = document.querySelector("#password_repeat");
-var LoginInput = document.getElementById("username-login");
-var PAGE_DATA = {};
-
-function showRegister() {
-    mainContent.style.display = "none";
-    registerForm.style.display = "block";
-    loginBtn.disabled = true;
-}
-function showLogin() {
-    mainContent.style.display = "none";
-    loginDiv.style.display = "block";
-    registerBtn.disabled = true;
-}
-function showProfileAfterRegister(event) {
-    var userName = document.querySelector("#username");
-    var passWord = document.querySelector("#password");
-    var passwordRepeat = document.querySelector("#password_repeat");
-    mainContent.style.display = "none";
-    registerForm.style.display = "none";
-    loginDiv.style.display = "none";
-    profile.style.display = "block";
-    event.preventDefault();
-    console.log(profile);
-    var welcome = document.getElementById("welcome-user");
-
-    welcome.innerHTML = userName.value;
-
-    fetch("https://bcca-pingpong.herokuapp.com/api/register/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8"
-        },
-        body: JSON.stringify({
-            username: userName.value,
-            password: passWord.value,
-            passwordRepeat: passwordRepeat.value
-        })
-    })
-        .then(r => r.json())
-        .then(obj => {
-            console.log(obj);
-        });
-}
-
-function showProfileAfterLogin(event) {
-    mainContent.style.display = "none";
-    registerForm.style.display = "none";
-    loginDiv.style.display = "none";
-    profile.style.display = "block";
-    event.preventDefault();
-    console.log({
-        username: userName.value,
-        password: passWord.value
-    });
-    var welcome = document.getElementById("welcome-user");
-
-    welcome.innerHTML = LoginInput.value;
-
-    fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8"
-        },
-        body: JSON.stringify({
-            username: userName.value,
-            password: passWord.value
-        })
-    })
-        .then(r => r.json())
-        .then(obj => {
-            PAGE_DATA.token = obj.token;
-            console.log(PAGE_DATA);
-        });
-}
-
-function addScore() {
+(function() {
+    var register = document.querySelector("#register");
+    var login = document.querySelector("#login");
+    var signUP = document.querySelector(".register");
+    var mainContent = document.getElementById("main-content");
+    var signIN = document.querySelector(".login");
+    var profile = document.querySelector("#profile");
+    var userName = document.querySelector("#name");
+    var passWord = document.querySelector("#code");
+    var passwordRepeat = document.querySelector("#code_repeat");
+    var name = document.getElementById("username");
+    var logPassword = document.getElementById("password");
     const score1 = document.getElementById("score-1");
     const score2 = document.getElementById("score-2");
-    var count = 0;
-    var score = 0;
+    var PAGE_DATA = {};
 
-    score1.addEventListener("click", function() {
-        count += 1;
-        score1.innerText = count;
-        if (score1.innerText === "10") {
-            score1.disabled = true;
-            score2.disabled = true;
-        }
-    });
+    function showRegister() {
+        mainContent.style.display = "none";
+        signUP.style.display = "block";
+        signIN.style.display = "none";
+    }
+    function showLogin() {
+        mainContent.style.display = "none";
+        signIN.style.display = "block";
+        signUP.style.display = "none";
+        register.disabled = true;
+    }
+    function showProfileAfterRegister(event) {
+        mainContent.style.display = "none";
+        signUP.style.display = "none";
+        signIN.style.display = "none";
+        profile.style.display = "block";
+        event.preventDefault();
+        console.log(profile);
+        var welcome = document.getElementById("welcome-user");
 
-    score2.addEventListener("click", () => {
-        score += 1;
-        score2.innerText = score;
-        if (score2.innerText === "10") {
-            score2.disabled = true;
-            score1.disabled = true;
-        }
-    });
-}
-function getInfo() {
-    fetch('https://bcca-pingpong.herokuapp.com/api/login/',
-    )
-}
-// fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
-//     method: "post",
-//     headers: {
-//         "Content-Type": "application/json;charset=utf-8"
-//     },
-//     body: JSON.stringify({ username: "Timothy", password: "password" })
-// })
-//     .then(r => r.json())
-//     .then(obj => {
-//         console.log(obj);
-//     });
+        welcome.innerHTML = userName.value;
 
-registerForm.addEventListener("submit", showProfileAfterRegister);
-loginDiv.addEventListener("submit", showProfileAfterLogin);
-registerBtn.addEventListener("click", showRegister);
-loginBtn.addEventListener("click", showLogin);
-addScore();
+        fetch("https://bcca-pingpong.herokuapp.com/api/register/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                username: userName.value,
+                password: passWord.value,
+                password_repeat: passwordRepeat.value
+            })
+        })
+            .then(r => r.json())
+            .then(obj => {
+                console.log(userName.value);
+                console.log(passWord.value);
+                console.log(passwordRepeat.value);
+                console.log(obj);
+            });
+    }
+
+    function showProfileAfterLogin() {
+        return fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                username: name.value,
+                password: logPassword.value
+            })
+        })
+            .then(r => r.json())
+            .then(obj => {
+                console.log(name.value);
+                console.log(logPassword.value);
+                PAGE_DATA.token = obj.token;
+                console.log(PAGE_DATA.token);
+                console.log(PAGE_DATA);
+            });
+    }
+
+    function addScore() {
+        var modal = document.querySelector("#myModal");
+        var winner = document.querySelector("#winner");
+        var count = 0;
+        var score = 0;
+
+        score1.addEventListener("click", function() {
+            count += 1;
+            score1.innerText = count;
+            if (score1.innerText === "10") {
+                modal.style.display = "block";
+                winner.innerHTML = "Player1 WINS!!!";
+            }
+        });
+
+        score2.addEventListener("click", () => {
+            score += 1;
+            score2.innerText = score;
+            if (score2.innerText === "10") {
+                modal.style.display = "block";
+                winner.innerHTML = "Player2 WINS!!!";
+            }
+        });
+        var span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", function() {
+            modal.style.display = "none";
+            score1.innerText = "0";
+            count = 0;
+            score2.innerText = "0";
+            score = 0;
+        });
+    }
+
+    function getUsers() {
+        var invalid = document.querySelectorAll("#invalid");
+        return fetch("https://bcca-pingpong.herokuapp.com/api/users/", {
+            method: "GET",
+            headers: {
+                Authorization: `Token ${PAGE_DATA.token}`
+            }
+        })
+            .then(function(r) {
+                return r.json();
+            })
+            .then(function(obj) {
+                if (!obj.token) {
+                    console.log("ERROR");
+                    name.classList.add("invalid");
+                    logPassword.classList.add("invalid");
+                    alert("Try Again");
+                }
+                return (PAGE_DATA.users = obj);
+            });
+    }
+    function userValidation(event) {
+        event.preventDefault();
+        showProfileAfterLogin()
+            .then(getUsers)
+            .then(function() {
+                usernameList = [];
+                for (var user of PAGE_DATA.users) {
+                    usernameList.push(user.username);
+                }
+                console.log(usernameList);
+                if (usernameList.includes(name.value)) {
+                    console.log("SUCCESS");
+                    mainContent.style.display = "none";
+                    signUP.style.display = "none";
+                    signIN.style.display = "none";
+                    profile.style.display = "block";
+                    // console.log({
+                    //     username: userName.value,
+                    //     password: passWord.value
+                    // });
+                    var welcome = document.getElementById("welcome-user");
+
+                    welcome.innerHTML = name.value;
+                }
+            });
+    }
+
+    // fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
+    //     method: "post",
+    //     headers: {
+    //         "Content-Type": "application/json;charset=utf-8"
+    //     },
+    //     body: JSON.stringify({ username: "Timothy", password: "password" })
+    // })
+    //     .then(r => r.json())
+    //     .then(obj => {
+    //         console.log(obj);
+    //     });
+
+    signUP.addEventListener("submit", showProfileAfterRegister);
+    signIN.addEventListener("submit", userValidation);
+    register.addEventListener("click", showRegister);
+    login.addEventListener("click", showLogin);
+    addScore();
+})();
