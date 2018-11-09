4bbada69cfd02abae6369ca869c4640b09791647
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-11-09 10:19:15 -0600

    made the program make a new gam and update the winners

--- functional_tests.py
+++ functional_tests.py
@@ -39,3 +39,40 @@ class TestLogin:
     def teardown_method(self):
         self.browser.quit()
 
+
+class TestRegister:
+    def setup_method(self):
+        self.browser = Firefox()
+        self.browser.get("http://localhost:5500/")
+
+    def test_register_success(self):
+        register = self.browser.find_element_by_id("register")
+        register.click()
+        username = self.browser.find_element_by_id("name")
+        password = self.browser.find_element_by_id("code")
+        passwordRepeat = self.browser.find_element_by_id("code_repeat")
+        button = self.browser.find_element_by_class_name("register-btn")
+        username.send_keys("oanvd")
+        password.send_keys("jjfasd")
+        passwordRepeat.send_keys("jjfasd")
+        button.click()
+        element = WebDriverWait(self.browser, 10).until(
+            Ec.text_to_be_present_in_element((By.ID, "welcome-user"), "oanvd"))
+        # assert "king" in element.get_attribute("innerText")
+
+    # def test_register_fail(self):
+    #     register = self.browser.find_element_by_id("register")
+    #     register.click()
+    #     username = self.browser.find_element_by_id("name")
+    #     password = self.browser.find_element_by_id("code")
+    #     passwordRepeat = self.browser.find_element_by_id("code_repeat")
+    #     button = self.browser.find_element_by_class_name("register-btn")
+    #     username.send_keys("king")
+    #     password.send_keys("james")
+    #     passwordRepeat.send_keys("james")
+    #     button.click()
+    #     element = WebDriverWait(self.browser, 10).until(
+    #         Ec.text_to_be_present_in_element((By.ID, "notAvailable"), ""))
+
+    def teardown_method(self):
+        self.browser.quit()
--- javascript.js
+++ javascript.js
@@ -4,7 +4,9 @@
     var signUP = document.querySelector(".register");
     var mainContent = document.getElementById("main-content");
     var signIN = document.querySelector(".login");
-    var profile = document.querySelector("#profile");
+    var userProfile = document.querySelector("#profile");
+    var game = document.querySelector(".game");
+    var newGame = document.getElementById("player-choice");
     var userName = document.querySelector("#name");
     var passWord = document.querySelector("#code");
     var passwordRepeat = document.querySelector("#code_repeat");
@@ -13,6 +15,67 @@
     const score1 = document.getElementById("score-1");
     const score2 = document.getElementById("score-2");
     var PAGE_DATA = {};
+    var logout = document.getElementById("logout");
+
+    // function profile() {
+    //     var player1 = document.querySelector(".player1");
+    //     var player2 = document.querySelector(".player2");
+    //     score1.disabled = true;
+    //     score2.disabled = true;
+    //     if (
+    //         player1.value === player2.value &&
+    //         player1.value !== "" &&
+    //         player2.value !== ""
+    //     ) {
+    //         var same = document.getElementById("same_name");
+    //         same.innerText = "They cannot be the same person";
+    //     }
+    // }
+
+    function createGame(event) {
+        event.preventDefault();
+        var user1 = document.querySelector("#user1");
+        var user2 = document.querySelector("#user2");
+        var name1 = document.querySelector(".player1").value;
+        var name2 = document.querySelector(".player2").value;
+
+        PAGE_DATA.player1Id = getId(name1);
+        PAGE_DATA.player2Id = getId(name2);
+
+        // console.log(PAGE_DATA.player1Id, PAGE_DATA.player2Id);
+
+        game.style.display = "block";
+        userProfile.style.display = "none";
+        user1.innerText = name1;
+        user2.innerText = name2;
+
+        // console.log(`token: ${PAGE_DATA.token}`);
+
+        fetch("https://bcca-pingpong.herokuapp.com/api/new-game/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+                Authorization: `Token ${PAGE_DATA.token}`
+            },
+            body: JSON.stringify({
+                player_1: PAGE_DATA.player1Id,
+                player_2: PAGE_DATA.player2Id
+            })
+        })
+            .then(r => r.json())
+            .then(obj => {
+                PAGE_DATA.gameId = obj.id;
+                PAGE_DATA.points = obj.points;
+            });
+    }
+
+    function getId(name) {
+        for (obj of PAGE_DATA.users) {
+            if (name == obj.username) {
+                return obj.id;
+            }
+        }
+    }
 
     function showRegister() {
         mainContent.style.display = "none";
@@ -25,8 +88,20 @@
         signUP.style.display = "none";
         register.disabled = true;
     }
-    function showProfileAfterRegister() {
-        fetch("https://bcca-pingpong.herokuapp.com/api/register/", {
+    function newGameAfterRegister() {
+        // event.preventDefault();
+        // mainContent.style.display = "none";
+        // signUP.style.display = "none";
+        // signIN.style.display = "none";
+        // userProfile.style.display = "block";
+        // console.log({
+        // //     username: userName.value,
+        // //     password: passWord.value
+        // // });
+        // var welcome = document.getElementById("welcome-user");
+
+        // welcome.innerHTML = userName.value;
+        return fetch("https://bcca-pingpong.herokuapp.com/api/register/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8"
@@ -39,14 +114,48 @@
         })
             .then(r => r.json())
             .then(obj => {
-                // console.log(userName.value);
-                // console.log(passWord.value);
-                // console.log(passwordRepeat.value);
-                // console.log(obj);
+                return new Promise((resolve, reject) => {
+                    if (obj.token) {
+                        PAGE_DATA.token = obj.token;
+                        // console.log("SUCCESS");
+                        mainContent.style.display = "none";
+                        signUP.style.display = "none";
+                        signIN.style.display = "none";
+                        userProfile.style.display = "block";
+                        logout.style.display = "block";
+
+                        // console.log({
+                        //     username: userName.value,
+                        //     password: passWord.value
+                        // });
+                        var welcome = document.getElementById("welcome-user");
+
+                        welcome.innerHTML = userName.value;
+                        resolve();
+                    } else {
+                        // console.log("OINASONSAODINSAOIN NO!!!!");
+                        userName.classList.add("invalid");
+                        passWord.classList.add("invalid");
+                        passwordRepeat.classList.add("invalid");
+                        reject();
+                    }
+                    // console.log(obj);
+                });
             });
     }
 
-    function showProfileAfterLogin() {
+    function newGameAfterLogin() {
+        // mainContent.style.display = "none";
+        // signUP.style.display = "none";
+        // signIN.style.display = "none";
+        // userProfile.style.display = "block";
+        // console.log({
+        // //     username: userName.value,
+        // //     password: passWord.value
+        // // });
+        // var welcome = document.getElementById("welcome-user");
+
+        // welcome.innerHTML = userName.value;
         return fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
             method: "POST",
             headers: {
@@ -69,7 +178,6 @@
 
     function getUsers() {
         var invalid = document.querySelector("#invalid");
-        var notAvailable = document.querySelector("#invalid");
         return fetch("https://bcca-pingpong.herokuapp.com/api/users/", {
             method: "GET",
             headers: {
@@ -77,46 +185,76 @@
             }
         })
             .then(function(r) {
-                console.log(PAGE_DATA.token);
+                // console.log(PAGE_DATA.token);
                 return r.json();
             })
             .then(function(obj) {
+                // console.log(obj.token);
                 if (!obj.token) {
-                    console.log("ERROR");
+                    // console.log("ERROR");
                     name.classList.add("invalid");
                     logPassword.classList.add("invalid");
-                    userName.classList.add("invalid");
-                    passWord.classList.add("invalid");
-                    passwordRepeat.classList.add("invalid");
-                    notAvailable.innerText =
-                        "Username is already taken, Please select another one";
+                    // userName.classList.add("invalid");
+                    // passWord.classList.add("invalid");
+                    // passwordRepeat.classList.add("invalid");
+                    // notAvailable.innerText =
+                    //     "Username is already taken, Please select another one";
                     invalid.innerText =
                         "Username or password is incorrect, Please try again.";
                 }
                 return (PAGE_DATA.users = obj);
             });
     }
-    function userValidation(event) {
+
+    function registerValidation(event) {
         event.preventDefault();
-        showProfileAfterRegister();
-        showProfileAfterLogin()
+        newGameAfterRegister()
             .then(getUsers)
             .then(function() {
                 usernameList = [];
-                console.log(PAGE_DATA);
+                // console.log(PAGE_DATA);
                 for (var user of PAGE_DATA.users) {
                     usernameList.push(user.username);
                 }
-                console.log(usernameList);
-                if (
-                    usernameList.includes(name.value) ||
-                    !usernameList.includes(userName.value)
-                ) {
-                    console.log("SUCCESS");
+                // console.log(usernameList);
+                for (var username of usernameList) {
+                    var datalist = document.getElementById("usernames");
+                    var option = document.createElement("option");
+                    option.innerText = username;
+                    datalist.appendChild(option);
+                }
+                // var player1 = document.querySelector(".player1");
+                // var player2 = document.querySelector(".player2");
+
+                // if (
+                //     player1.value !== player2.value &&
+                //     player1.value !== "" &&
+                //     player2.value !== ""
+                // ) {
+                //     score1.removeAttribute("disabled");
+                //     score2.removeAttribute("disabled");
+                // }
+            });
+    }
+
+    function logInValidation(event) {
+        event.preventDefault();
+        newGameAfterLogin()
+            .then(getUsers)
+            .then(function() {
+                usernameList = [];
+                // console.log(PAGE_DATA);
+                for (var user of PAGE_DATA.users) {
+                    usernameList.push(user.username);
+                }
+                // console.log(usernameList);
+                if (usernameList.includes(name.value)) {
+                    // console.log("SUCCESS");
                     mainContent.style.display = "none";
                     signUP.style.display = "none";
                     signIN.style.display = "none";
-                    profile.style.display = "block";
+                    userProfile.style.display = "block";
+                    logout.style.display = "block";
                     // console.log({
                     //     username: userName.value,
                     //     password: passWord.value
@@ -131,41 +269,85 @@
                     option.innerText = username;
                     datalist.appendChild(option);
                 }
+                // var player1 = document.querySelector(".player1");
+                // var player2 = document.querySelector(".player2");
+
+                // if (
+                //     player1.value !== player2.value &&
+                //     player1.value !== "" &&
+                //     player2.value !== ""
+                // ) {
+                //     score1.removeAttribute("disabled");
+                //     score2.removeAttribute("disabled");
+                // }
             });
     }
 
-    // function addScore() {
-    //     var modal = document.querySelector("#myModal");
-    //     var winner = document.querySelector("#winner");
-    //     var count = 0;
-    //     var score = 0;
+    function submitScore() {
+        fetch(
+            `https://bcca-pingpong.herokuapp.com/api/score-game/${
+                PAGE_DATA.gameId
+            }/`,
+            {
+                method: "PUT",
+                headers: {
+                    Authorization: `Token ${PAGE_DATA.token}`,
+                    "Content-Type": "application/json;charset=utf-8"
+                },
+                body: JSON.stringify({
+                    points: PAGE_DATA.points
+                })
+            }
+        )
+            .then(r => r.json())
+            .then(obj => {
+                // console.log(obj);
+            });
+    }
 
-    //     score1.addEventListener("click", function() {
-    //         count += 1;
-    //         score1.innerText = count;
-    //         if (score1.innerText === "10") {
-    //             modal.style.display = "block";
-    //             winner.innerHTML = "Player1 WINS!!!";
-    //         }
-    //     });
+    function addScore() {
+        var modal = document.querySelector("#myModal");
+        var winner = document.querySelector("#winner");
+        var count = 0;
+        var score = 0;
+        var player1 = document.querySelector(".player1");
+        var player2 = document.querySelector(".player2");
 
-    //     score2.addEventListener("click", () => {
-    //         score += 1;
-    //         score2.innerText = score;
-    //         if (score2.innerText === "10") {
-    //             modal.style.display = "block";
-    //             winner.innerHTML = "Player2 WINS!!!";
-    //         }
-    //     });
-    //     var span = document.getElementsByClassName("close")[0];
-    //     span.addEventListener("click", function() {
-    //         modal.style.display = "none";
-    //         score1.innerText = "0";
-    //         count = 0;
-    //         score2.innerText = "0";
-    //         score = 0;
-    //     });
-    // }
+        score1.addEventListener("click", function() {
+            count += 1;
+            score1.innerText = count;
+            PAGE_DATA.points.push(PAGE_DATA.player1Id);
+
+            if (score1.innerText === "10") {
+                modal.style.display = "block";
+                winner.innerHTML = player1.value + " WINS!!!";
+                submitScore();
+            }
+        });
+
+        score2.addEventListener("click", () => {
+            score += 1;
+            score2.innerText = score;
+            PAGE_DATA.points.push(PAGE_DATA.player2Id);
+            if (score2.innerText === "10") {
+                modal.style.display = "block";
+                winner.innerHTML = player2.value + " WINS!!!";
+                submitScore();
+            }
+        });
+        var span = document.getElementsByClassName("close")[0];
+        span.addEventListener("click", function() {
+            modal.style.display = "none";
+            score1.innerText = "0";
+            player1.value = "";
+            count = 0;
+            score2.innerText = "0";
+            player2.value = "";
+            score = 0;
+            game.style.display = "none";
+            userProfile.style.display = "block";
+        });
+    }
 
     // fetch("https://bcca-pingpong.herokuapp.com/api/login/", {
     //     method: "post",
@@ -176,12 +358,14 @@
     // })
     //     .then(r => r.json())
     //     .then(obj => {
-    //         console.log(obj);
+    // console.log(obj);
     //     });
 
-    signUP.addEventListener("submit", userValidation);
-    signIN.addEventListener("submit", userValidation);
+    signUP.addEventListener("submit", registerValidation);
+    signIN.addEventListener("submit", logInValidation);
     register.addEventListener("click", showRegister);
     login.addEventListener("click", showLogin);
-    // addScore();
+    newGame.addEventListener("submit", createGame);
+    // profile();
+    addScore();
 })();
